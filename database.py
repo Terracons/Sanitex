6741ef3e51f950b0f64baef03f47dcb0f99746bb
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2025-03-30 22:15:53 +0100

    next

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///database.sqlite3"
+DATABASE_URL = "postgresql://postgres:sg201404754@localhost/booking_services"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
